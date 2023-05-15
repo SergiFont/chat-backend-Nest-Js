78361cd74213d9c7e1431b0f747e666bb7003aa1
SergiFont <sfontsmx2010@gmail.com>
2023-05-15 20:56:24 +0200

    yes

--- src/message-ws/interfaces/ConnectedClients.infertace.ts
+++
@@ -1,9 +0,0 @@
-import { Socket } from 'socket.io';
-import { User } from '../../auth/entities/user.entity';
-
-export interface ConnectedClients{
-    [id: string]: {
-        socket: Socket,
-        user: User
-    }
-}
